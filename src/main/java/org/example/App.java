9be18dc62a8f0b54f9dab62bd0cc8bec853c2973
AndreasDevanPanduNarasamdya <andreasdevan123@gmail.com>
2025-09-28 03:11:46 +0700

    Small Ass Update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,4 +1,7 @@
 package org.example;
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.win32.W32APIOptions;
 
 /**
  * Hello world!
@@ -7,4 +10,10 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public interface User32 extends Library
+    {
+        
+    }
+
 }
