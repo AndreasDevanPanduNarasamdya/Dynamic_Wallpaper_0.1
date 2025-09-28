769adfe0b240f858f628d1e5a52a6b2e52549a7f
AndreasDevanPanduNarasamdya <andreasdevan123@gmail.com>
2025-09-28 17:00:48 +0700

    update on java swing and jna

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,10 @@
 package org.example;
 import com.sun.jna.Library;
-import com.sun.jna.Native;
-import com.sun.jna.win32.W32APIOptions;
 
 /**
  * Hello world!
  */
-public class App {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
---
+++ src/main/java/org/example/gui/WallpaperGUI.java
@@ -0,0 +1,13 @@
+package org.example.gui;
+
+import javax.swing.*;
+
+public class WallpaperGUI {
+    private JPanel panel1;
+    private JButton button1;
+    private JButton button2;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
